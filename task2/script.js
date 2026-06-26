7e83150485e080f427ae073b2c2ae0764acd5c65
sarika2201 <sarikasaini2207@gmail.com>
2026-06-26 11:56:00 +0530

    random colour generator

---
+++ task2/script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+    
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    
+    document.body.style.backgroundColor = randomColor;
+});
